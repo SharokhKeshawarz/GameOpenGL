eb99750cee4e6778183d498bcbdcdd500c9ad1a3
KeshawarzSharokh <ahamad.keshawarz@epitech.eu>
2024-05-30 23:15:39 +0200

    [+] Setup

---
+++ include/main.hpp
@@ -0,0 +1,23 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+
+#include "glad.h"
+
+#include "GLFW/glfw3.h"
+
+#include "glm/vec3.hpp"
+#include "glm/vec4.hpp"
+#include "glm/mat4x4.hpp"
+#include "glm/ext/matrix_transform.hpp"
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/scalar_constants.hpp"
+
+#include "stb_image.h"
+
+#endif // MAIN_HPP
